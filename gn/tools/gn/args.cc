33583e087c7c043a69ad8d1f0d907108ff7055d3
local <local@Mac-402324.local>
2021-02-02 14:27:52 -0800

    add arm64 host

--- gn/tools/gn/args.cc
+++ gn/tools/gn/args.cc
@@ -338,8 +338,10 @@ void Args::SetSystemVarsLocked(Scope* dest) const {
     arch = kX86;
   else if (os_arch == "x86_64")
     arch = kX64;
-  else if (os_arch.substr(0, 3) == "arm")
+  else if (os_arch == "arm")
     arch = kArm;
+  else if (os_arch == "arm64")
+    arch = kArm64;
   else if (os_arch == "aarch64")
     arch = kArm64;
   else if (os_arch == "mips")
