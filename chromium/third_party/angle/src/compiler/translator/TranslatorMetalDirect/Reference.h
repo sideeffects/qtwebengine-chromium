4b55ed9a7f97a1625aea2540ba1aa1982541ddec
Robert Vinluan <rvinluan@sidefx.com>
2024-10-28 16:06:46 -0400

    Add patches to fix Chromium build with Xcode 15.3 + macOS 14.4 SDK.
    The first two patches I pulled from the FreeBSD ports project:
    https://cgit.freebsd.org/ports/commit/?id=660b01aa59ee1266ce0faf5718dc5c90abfa4f31
    
    They fix errors about '__is_cpp17_contiguous_iterator'.
    
    The third patch I pulled from upstream qtwebengine-chromium:
    https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/577667
    
    It fixes errors about spellcheck_buildflags.h not existing.
    I only needed the change to ./chromium/content/public/browser/BUILD.gn.
    The other changed file in the upstream commit does not exist in Qt 6.5's
    chromium version.

--- chromium/third_party/angle/src/compiler/translator/TranslatorMetalDirect/Reference.h
+++ chromium/third_party/angle/src/compiler/translator/TranslatorMetalDirect/Reference.h
@@ -35,8 +35,8 @@ class Ref
     operator T &() { return *mPtr; }
     operator T const &() const { return *mPtr; }
 
-    operator T *() { return *mPtr; }
-    operator T const *() const { return *mPtr; }
+    operator T *() { return mPtr; }
+    operator T const *() const { return mPtr; }
 
   private:
     T *mPtr;
